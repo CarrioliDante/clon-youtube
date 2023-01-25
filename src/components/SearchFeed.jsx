05d42b0ea454317d7e52fe3c9ec58c483aacb741
CarrioliDante <carriolidante@gmail.com>
2023-01-25 17:38:59 -0300

    .

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,25 @@
+import {useState, useEffect} from "react";
+import {Typography, Box} from "@mui/material";
+import {useParams} from "react-router-dom";
+import {fetchFromAPI} from "../utils/fetchFromAPI";
+import {Videos} from "./";
+
+const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const {searchTerm} = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => setVideos(data.items));
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} minHeight='95vh'>
+      <Typography variant='h4' fontWeight={900} color='white' mb={2} ml={{sm: "100px"}}>
+        Resultados de <span style={{color: "#FC1503"}}>{searchTerm}</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
