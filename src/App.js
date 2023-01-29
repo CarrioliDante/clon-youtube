f6ee416bff2d71a14381c11b8ae9280aa186fb62
CarrioliDante <carriolidante@gmail.com>
2023-01-29 16:31:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import {Box} from "@mui/material";
 import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from "./components";
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename='/clon-youtube'>
     <Box sx={{backgroundColor: "#000"}}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
-        <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/channel/:id" element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
